8f182b94dfe8f478351e7d93d984e44669a4d578
Anton Lukashin <lukan@itcwin.com>
2013-10-01 16:13:04 +0400

    Second and third lessons

---
+++ java/src/ru/ics/spbstu/java/initiallection/First.java
@@ -0,0 +1,45 @@
+package ru.ics.spbstu.java.initiallection;
+
+import java.util.List;
+import java.util.Vector;
+
+
+public class First {
+	public static void main(String[] args) {
+		System.out.println("Hello World!!!");
+
+		byte b;
+		Byte bb;
+		int i;
+		Integer ii;
+		short s;
+		Short ss;
+		long l;
+		Long ll;
+		float f;
+		Float ff;
+		double d;
+		Double dd;
+		char c;
+		Character cc;
+		boolean bool = true;
+		Boolean bbb = Boolean.FALSE;
+
+		String ssss = "taram param";
+		String sssss = new String("Purum pum pum");
+		ssss += "ura";
+		Integer int1 = 6;
+		Integer int2 = 6;
+		System.out.println("Equals? " + (int1 == int2));
+		int int3 = 1262;
+		int int4 = 1262;
+		System.out.println("Equals? " + (int3 == int4));
+		int[] a = new int[4];
+		
+		Vector<String> vector;
+		Vector vector2;
+		Object obj = new Object();
+		List<Integer> list;
+		
+	}
+}
